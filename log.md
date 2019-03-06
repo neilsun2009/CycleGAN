a7835798dcb20f2d5bceb21bf6e9356ca8f87a21
unknown <neilsun2009@163.com>
2019-03-07 01:19:35 +0800

    [add] identity mapping & log

---
+++ log.md
@@ -0,0 +1,20 @@
+# Log for Cycle GAN Project
+> Please use this Markdown to document an thoughts or progress, hopefully it will eventually convert into the final paper
+
+## Abstract
+
+## Background
+
+## Dataset
+
+## Implementation
++ Identity mapping to ensure the color of the original image
+
+## Training
+
+## Results
+
+## Summary
+
+## References
++ Intuition behind the identity mapping loss. https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/issues/322
--- src/model.py
+++ src/model.py
@@ -29,15 +29,13 @@ class CycleGAN():
     self.disc_b = self.build_discriminator('disc_b')
     self.gen_a2b = self.build_generator('gen_a2b')
     self.gen_b2a = self.build_generator('gen_b2a')
-    self.combined_a = self.build_combined(self.gen_a2b, self.gen_b2a, self.disc_b)
-    self.combined_b = self.build_combined(self.gen_b2a, self.gen_a2b, self.disc_a)
+    self.combined = self.build_combined(self.gen_a2b, self.gen_b2a, self.disc_a, self.disc_b)
     if self.verbose:
       self.disc_a.summary()
       self.disc_b.summary()
       self.gen_a2b.summary()
       self.gen_b2a.summary()
-      self.combined_a.summary()
-      self.combined_b.summary()
+      self.combined.summary()
 
   def build_discriminator(self, name_base):
     img = KL.Input(self.img_shape)
@@ -49,12 +47,20 @@ class CycleGAN():
     output = self.base.generator(img, name_base)
     return KM.Model(img, output)
   
-  def build_combined(self, gen1, gen2, disc):
-    img = KL.Input(shape=self.img_shape)
-    fake = gen1(img)
-    fake_fake = gen2(fake)
-    disc_out = disc(fake)
-    return KM.Model(img, [disc_out, fake_fake])
+  def build_combined(self, gen_a2b, gen_b2a, disc_a, disc_b):
+    img_a = KL.Input(shape=self.img_shape)
+    img_b = KL.Input(shape=self.img_shape)
+    fake_b = gen_a2b(img_a)
+    fake_a = gen_b2a(img_b)
+    fake_fake_a = gen_b2a(fake_b)
+    fake_fake_b = gen_a2b(fake_a)
+    disc_out_a = disc_a(fake_a)
+    disc_out_b = disc_b(fake_b)
+    # identity mapping
+    id_a = gen_b2a(img_a)
+    id_b = gen_a2b(img_b)
+    return KM.Model([img_a, img_b], [disc_out_a, disc_out_b, 
+      fake_fake_a, fake_fake_b, id_a, id_b])
   
   def compile(self, learning_rate):
     adam = keras.optimizers.Adam(learning_rate, 0.5)
@@ -70,12 +76,10 @@ class CycleGAN():
     #   layer.trainable = False
     self.disc_a.trainable = False
     self.disc_b.trainable = False
-    self.combined_a.compile(loss=['mse', 'mae'],
-      loss_weights=[1, 10], optimizer=adam)
-    self.combined_b.compile(loss=['mse', 'mae'],
-      loss_weights=[1, 10], optimizer=adam)
+    self.combined.compile(loss=['mse', 'mse', 'mae', 'mae', 'mae', 'mae'],
+      loss_weights=[1, 1, 10, 10, 1, 1], optimizer=adam)
     # print(n_disc_trainable, len(self.disc_a._collected_trainable_weights))
-    # print(len(self.combined_a._collected_trainable_weights), len(self.gen_a2b.trainable_weights))
+    # print(len(self.combined._collected_trainable_weights), len(self.gen_a2b.trainable_weights))
   
   def train(self, path_a, path_b, epochs=100, steps_per_epoch=1000, 
       batch_size=1, save_image_every_step=50, save_model_every_epoch=1,
@@ -86,8 +90,7 @@ class CycleGAN():
     if load_model:
       self.disc_a = KM.load_model(model_load_path + 'model-disc-a.h5')
       self.disc_b = KM.load_model(model_load_path + 'model-disc-b.h5')
-      self.combined_a = KM.load_model(model_load_path + 'model-gan-a.h5')
-      self.combined_b = KM.load_model(model_load_path + 'model-gan-b.h5')
+      self.combined = KM.load_model(model_load_path + 'model-gan.h5')
       print('Loaded weights from', model_load_path)
     ones = np.ones((batch_size,) + self.base.DISC_OUTPUT_SIZE)
     zeros = np.zeros((batch_size,) + self.base.DISC_OUTPUT_SIZE)
@@ -114,14 +117,14 @@ class CycleGAN():
         disc_b_loss_fake = self.disc_b.train_on_batch(fake_b, zeros)
         disc_b_loss = 0.5 * np.add(disc_b_loss_fake, disc_b_loss_real)
         # train generators
-        gen_a_loss = self.combined_a.train_on_batch(img_a, [ones, img_a])
-        gen_b_loss = self.combined_b.train_on_batch(img_b, [ones, img_b])
+        gen_loss = self.combined.train_on_batch([img_a, img_b], [ones, ones, 
+          img_a, img_b, img_a, img_b])
         # plot
-        print ("[Epoch %d/%d] [Batch %d/%d] [D_A loss: %f, acc: %3d%%] [D_B loss: %f, acc: %3d%%] [G_A loss: %f] [G_B loss: %f] time: %s" % (epoch+1, epochs,
+        print ("[Epoch %d/%d] [Batch %d/%d] [D_A loss: %f, acc: %3d%%] [D_B loss: %f, acc: %3d%%] [G loss: %f] time: %s" % (epoch+1, epochs,
                 step+1, steps_per_epoch,
                 disc_a_loss[0], 100*disc_a_loss[1],
                 disc_b_loss[0], 100*disc_b_loss[1],
-                gen_a_loss[0], gen_b_loss[0],
+                gen_loss[0], 
                 time.time() - start_time))
         # save image
         if step % save_image_every_step == 0:
@@ -144,7 +147,6 @@ class CycleGAN():
       if epoch % save_model_every_epoch == 0:
         self.disc_a.save(model_save_path + 'model-disc-a.h5')
         self.disc_b.save(model_save_path + 'model-disc-b.h5')
-        self.combined_a.save(model_save_path + 'model-gan-a.h5')
-        self.combined_b.save(model_save_path + 'model-gan-b.h5')
+        self.combined.save(model_save_path + 'model-gan.h5')
         print('Saved model for epoch', epoch+1)
 
