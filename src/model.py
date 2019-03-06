345a4f877f89d66ca9ae9c784ad812d5a2aeaa20
unknown <neilsun2009@163.com>
2019-03-06 13:42:47 +0800

    [update] model utils

--- src/model.py
+++ src/model.py
@@ -13,7 +13,6 @@ import time
 import data_utils
 import matplotlib.pyplot as plt
 import PIL
-from IPython.display import clear_output
 
 class CycleGAN():
 
@@ -59,22 +58,37 @@ class CycleGAN():
   
   def compile(self, learning_rate):
     adam = keras.optimizers.Adam(learning_rate, 0.5)
-    self.disc_a.compile(loss='mse',
+    # n_disc_trainable = len(self.disc_a.trainable_weights)
+    self.disc_a.compile(loss='mse', # keras.losses.binary_crossentropy,
       optimizer=adam, metrics=['accuracy'])
-    self.disc_b.compile(loss='mse',
+    self.disc_b.compile(loss='mse', # keras.losses.binary_crossentropy,
       optimizer=adam, metrics=['accuracy'])
     # don't update discrimators during the training of generators
-    for layer in self.disc_a.layers:
-      layer.trainable = False
-    for layer in self.disc_b.layers:
-      layer.trainable = False
+    # for layer in self.disc_a.layers:
+    #   layer.trainable = False
+    # for layer in self.disc_b.layers:
+    #   layer.trainable = False
+    self.disc_a.trainable = False
+    self.disc_b.trainable = False
     self.combined_a.compile(loss=['mse', 'mae'],
       loss_weights=[1, 10], optimizer=adam)
     self.combined_b.compile(loss=['mse', 'mae'],
       loss_weights=[1, 10], optimizer=adam)
+    # print(n_disc_trainable, len(self.disc_a._collected_trainable_weights))
+    # print(len(self.combined_a._collected_trainable_weights), len(self.gen_a2b.trainable_weights))
   
-  def train(self, path_a, path_b, epochs=100, steps_per_epoch=100, batch_size=1, save_image_every=10):
+  def train(self, path_a, path_b, epochs=100, steps_per_epoch=1000, 
+      batch_size=1, save_image_every_step=50, save_model_every_epoch=1,
+      image_save_path='../output/images/', model_save_path='../models/',
+      show_image=True, load_model=False, model_load_path='../models/'):
     print('Training starts...')
+    # loading model
+    if load_model:
+      self.disc_a = KM.load_model(model_load_path + 'model-disc-a.h5')
+      self.disc_b = KM.load_model(model_load_path + 'model-disc-b.h5')
+      self.combined_a = KM.load_model(model_load_path + 'model-gan-a.h5')
+      self.combined_b = KM.load_model(model_load_path + 'model-gan-b.h5')
+      print('Loaded weights from', model_load_path)
     ones = np.ones((batch_size,) + self.base.DISC_OUTPUT_SIZE)
     zeros = np.zeros((batch_size,) + self.base.DISC_OUTPUT_SIZE)
     data_a = data_utils.get_train_dataset(path_a)
@@ -103,14 +117,17 @@ class CycleGAN():
         gen_a_loss = self.combined_a.train_on_batch(img_a, [ones, img_a])
         gen_b_loss = self.combined_b.train_on_batch(img_b, [ones, img_b])
         # plot
-        print ("[Epoch %d/%d] [Batch %d/%d] [D_A loss: %f, acc: %3d%%] [D_B loss: %f, acc: %3d%%] [G_A loss: %f] [G_B loss: %f] time: %s" % (epoch, epochs,
-                step, steps_per_epoch,
+        print ("[Epoch %d/%d] [Batch %d/%d] [D_A loss: %f, acc: %3d%%] [D_B loss: %f, acc: %3d%%] [G_A loss: %f] [G_B loss: %f] time: %s" % (epoch+1, epochs,
+                step+1, steps_per_epoch,
                 disc_a_loss[0], 100*disc_a_loss[1],
                 disc_b_loss[0], 100*disc_b_loss[1],
                 gen_a_loss[0], gen_b_loss[0],
                 time.time() - start_time))
-        if step % save_image_every == 0:
-          clear_output(wait=True)
+        # save image
+        if step % save_image_every_step == 0:
+          if show_image:
+            from IPython.display import clear_output
+            clear_output(wait=True)
           plt.figure(figsize=(15,15))
           titles = ['Input A', 'Output A', 'Input B', 'Output B']
           displays = [img_a, fake_b, img_b, fake_a]
@@ -118,10 +135,16 @@ class CycleGAN():
             plt.subplot(2, 2, i+1)
             plt.title(titles[i])
             display = displays[i][0]
-            # print(display.shape)
-            # print(displays[i].shape)
-            # print(np.min(display), np.max(display))
-            # getting the pixel values between [0, 1] to plot it.
             plt.imshow(display * 0.5 + 0.5)
             plt.axis('off')
+          plt.savefig('%s%d-%d.jpg' % (image_save_path, epoch+1, step+1))
+          if show_image:
             plt.show()
+      # save model
+      if epoch % save_model_every_epoch == 0:
+        self.disc_a.save(model_save_path + 'model-disc-a.h5')
+        self.disc_b.save(model_save_path + 'model-disc-b.h5')
+        self.combined_a.save(model_save_path + 'model-gan-a.h5')
+        self.combined_b.save(model_save_path + 'model-gan-b.h5')
+        print('Saved model for epoch', epoch+1)
+
